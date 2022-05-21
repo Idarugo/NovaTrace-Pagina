a70a1160455a55d8b52daaefd3709a4b2d2045ae
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2022-05-21 13:26:13 -0400

    bg Mod

--- control_sesion.php
+++ control_sesion.php
@@ -15,8 +15,57 @@ if (isset($_POST["btnvalida"])) {
     
     echo "<script>window.location = 'login.html'</script>";
   }
-}
- ?> 
+}else{ 
+          echo '<h6 style="color:red">Error al validar el usuario y contraseña</h6>'; 
+      } 
+
+  
+ if(isset($_POST['btn_ing'])){ 
+     $validacion = false; 
+     if(strlen($_POST['txt_rut']) >= 1 && strlen($_POST['txt_pass'] >= 1)){ 
+  
+     require 'Class/DAO.php'; 
+     $d = new DAO(); 
+  
+     $txt_rut = trim($_POST['txt_rut']); 
+     $txt_pas = trim($_POST['txt_pass']); 
+  
+     $lista = $d->ListarSesion(); 
+     $rut = ""; $pas = ""; $est=""; 
+  
+     for ($i=0; $i < count($lista); $i++) {  
+         $u = $lista[$i]; 
+         $rut = $u->getRut(); 
+         $pas = $u->getClave(); 
+         $tip = $u->getTipo(); 
+         if($rut == $txt_rut and $pas == $txt_pas){ 
+             $validacion = true; 
+             $tipo = $tip; 
+              
+         } 
+     } 
+ } 
+  
+      
+     if($validacion == true){ 
+         session_name('data'); 
+         session_start(); 
+         $_SESSION['rut'] = $txt_rut; 
+         $_SESSION['tipo'] = $tipo; 
+         if($tipo == "ADMINISTRADOR"){ 
+             echo '<script> window.location.replace("menuA.php");  
+         </script>'; 
+         }else{ 
+             echo '<script> window.location.replace("menu.php");  
+         </script>'; 
+         } 
+          
+          
+     }else{ 
+         echo '<h6 style="color:red">Error al validar el usuario y contraseña</h6>'; 
+     } 
+ } 
+ ?>
 
 
 
