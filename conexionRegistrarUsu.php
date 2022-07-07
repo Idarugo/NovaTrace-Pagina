10c9628daeeb2ba0e99b7e6a4bd1e28315ec2b61
Idarugo618 <onepiecemonkeydluffy20@gmail.com>
2022-07-07 02:42:30 -0400

    Avance

--- conexionRegistrarUsu.php
+++ conexionRegistrarUsu.php
@@ -1,6 +1,6 @@
 
 <?php 
-    $con = mysqli_connect("us-cdbr-east-06.cleardb.net","bd2afeddb63b20","df20e8da","heroku_a50bea369876c7e"));
+    $con = mysqli_connect("us-cdbr-east-06.cleardb.net","bd2afeddb63b20","df20e8da","heroku_a50bea369876c7e");
 
 	if(!$con){
 	echo "no pudo conectarse al sv";
